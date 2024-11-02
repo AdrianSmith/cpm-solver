639d8a9e8622c6e1f241bbe5c61d8f15ba31536b
Adrian Smith <adrianlloydsmith@gmail.com>
2024-11-02 10:10:38 +1000

    Resolve critical path test

--- spec/cpm_solver/csv_reader_spec.rb
+++ spec/cpm_solver/csv_reader_spec.rb
@@ -22,10 +22,16 @@ RSpec.describe CpmSolver::CsvReader do
     end
 
     it "should have 1 end activity" do
-      puts program.end_activities
       expect(program.end_activities.size).to eq(1)
     end
 
-    it "calculates the correct critical path"
+    it "calculates the correct critical path" do
+      expected_critical = %w[
+        1 2 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
+        21 22 23 37 38 39 46 47 48 49 50 51 52 54 55 56 59 61 82 92 99 100
+      ]
+      actual_critical = program.critical_path_activities.keys
+      expect(actual_critical).to eq(expected_critical)
+    end
   end
 end
